d2156bc5ac0de3b2aa96bd139243689c44845bf0
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-08-12 23:31:35 +0700

    feat: add header and install dependencies

--- src/components/templates/nav.tsx
+++ src/components/templates/nav.tsx
@@ -1,5 +1,13 @@
 export default function Navigation() {
     return (
-        <div></div>
+        <div className="flex flex-col items-start justify-center py-6 pl-[120px]">
+            <div className="max-w-[1200px] flex flex-row items-center">
+                <div className="text-[14px] leading-[160%] text-base-white px-4 py-[7px] bg-white-neutral-900 rounded-[8px]">Overview</div>
+                <div className="text-[14px] leading-[160%] text-white-neutral-400 px-4 py-[7px]">Tokens</div>
+                <div className="text-[14px] leading-[160%] text-white-neutral-400 px-4 py-[7px]">Pools</div>
+                <div className="text-[14px] leading-[160%] text-white-neutral-400 px-4 py-[7px]">Portfolio</div>
+                <div className="text-[14px] leading-[160%] text-white-neutral-400 px-4 py-[7px]">Settings</div>
+            </div>
+        </div>
     )
 }
---
+++ src/pages/overview.tsx
@@ -0,0 +1,7 @@
+export default function Overview() {
+    return (
+        <div className="flex flex-col items-center justify-center px-[120px]">
+            <div className="max-w-[1200px] grid grid-cols-12"></div>
+        </div>
+    )
+}
