7fcd5a3d2e853747c9b95a3d514e1b3d34bc22b9
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-08-15 10:14:15 +0700

    feat: add pools page

---
+++ src/data/pool.ts
@@ -0,0 +1,97 @@
+import usdc from "@/assets/pools/usdc.png"
+import eth from "@/assets/pools/ethp.png"
+import usdt from "@/assets/pools/usdt.png"
+import dai from "@/assets/pools/dai.png"
+
+export const PoolsData = [
+    {
+        token1: { name: "USDC", image: usdc },
+        token2: { name: "ETH", image: eth },
+        version: "V3",
+        change: "0.05%",
+        tvl: "$155.4M",
+        volume: "$145.5M",
+        apr: "17.088%"
+    },
+    {
+        token1: { name: "ETH", image: eth },
+        token2: { name: "USDT", image: usdt },
+        version: "V3",
+        change: "0.3%",
+        tvl: "$78.8M",
+        volume: "$28.5M",
+        apr: "39.561%"
+    },
+    {
+        token1: { name: "DAI", image: dai },
+        token2: { name: "USDC", image: usdc },
+        version: "V3",
+        change: "0.01%",
+        tvl: "$82.8M",
+        volume: "$80.1M",
+        apr: "13.021%"
+    },
+    {
+        token1: { name: "USDC", image: usdc },
+        token2: { name: "DAI", image: dai },
+        version: "V3",
+        change: "0.7%",
+        tvl: "$183.8M",
+        volume: "$112.5M",
+        apr: "32.002%"
+    },
+    {
+        token1: { name: "USDC", image: usdc },
+        token2: { name: "USDT", image: usdt },
+        version: "V3",
+        change: "0.8%",
+        tvl: "$83.9M",
+        volume: "$22.5M",
+        apr: "3.021%"
+    },
+    {
+        token1: { name: "ETH", image: eth },
+        token2: { name: "USDC", image: usdc },
+        version: "V3",
+        change: "1.82%",
+        tvl: "$43.2M",
+        volume: "$8.5M",
+        apr: "9.002%"
+    },
+    {
+        token1: { name: "USDC", image: usdc },
+        token2: { name: "DAI", image: dai },
+        version: "V3",
+        change: "1.9%",
+        tvl: "$21.8M",
+        volume: "$12.5M",
+        apr: "32.112%"
+    },
+    {
+        token1: { name: "USDC", image: usdc },
+        token2: { name: "ETH", image: eth },
+        version: "V3",
+        change: "2.3%",
+        tvl: "$53.1M",
+        volume: "$43.5M",
+        apr: "11.933%"
+    },
+    {
+        token1: { name: "ETH", image: eth },
+        token2: { name: "USDC", image: usdc },
+        version: "V3",
+        change: "0.03%",
+        tvl: "$92.2M",
+        volume: "$90.5M",
+        apr: "12.932%"
+    },
+    {
+        token1: { name: "USDT", image: usdt },
+        token2: { name: "ETH", image: eth },
+        version: "V3",
+        change: "0.93%",
+        tvl: "$88.8M",
+        volume: "$21.5M",
+        apr: "30.921%"
+    }
+]
