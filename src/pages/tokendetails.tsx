92162cd87a94ed9e8fc2bdf4fb621c72a911e5b1
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-09-02 13:27:50 +0700

    style: adjust text on price chart

--- src/pages/tokendetails.tsx
+++ src/pages/tokendetails.tsx
@@ -1,9 +1,9 @@
 import Swap from "@/components/overview/swap";
-import TotalBalance from "@/components/overview/totalbalance/totalbalance";
 import SwapCrypto from "@/components/tokendetail/crypto";
 import DetailsTokenDetail from "@/components/tokendetail/details";
 import MarketTokenDetail from "@/components/tokendetail/market";
 import PriceTokenDetail from "@/components/tokendetail/price";
+import PriceToken from "@/components/tokendetail/pricetoken";
 import SwapSentiment from "@/components/tokendetail/sentiment";
 import TransactionTokenDetail from "@/components/tokendetail/transaction";
 
@@ -16,7 +16,7 @@ export default function TokenDetails() {
             <div className="max-w-[1200px] w-full grid grid-cols-12 gap-5">
                 <div className="col-span-8 flex flex-col gap-5">
                     <div className="w-full h-[348px]">
-                        <TotalBalance />
+                        <PriceToken />
                     </div>
                     <MarketTokenDetail />
                     <DetailsTokenDetail />
