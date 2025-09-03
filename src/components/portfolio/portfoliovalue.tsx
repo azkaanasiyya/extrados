cb9c57a1a813859d9066b5255505accb8cadac37
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-09-03 11:24:43 +0700

    style: adjust tooltip on chart

--- src/components/portfolio/portfoliovalue.tsx
+++ src/components/portfolio/portfoliovalue.tsx
@@ -3,40 +3,40 @@ import { useState } from 'react';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { ChartContainer, ChartTooltip } from '@/components/ui/chart';
-import { Line, LineChart, XAxis, YAxis, type TooltipProps } from 'recharts';
+import { CartesianGrid, Line, LineChart, XAxis, YAxis, type TooltipProps } from 'recharts';
 import type { ChartConfig } from '@/components/ui/chart';
 import { TrendingUp } from 'lucide-react';
 import type { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent';
 
 const portfolioData = [
-    { date: '18 Oct', value: 7, price: '9,865' },
-    { date: '18 Oct', value: -1, price: '3,896' },
-    { date: '18 Oct', value: 12, price: '100' },
-    { date: '18 Oct', value: 8, price: '7,084' },
-    { date: '19 Oct', value: 5, price: '250' },
-    { date: '19 Oct', value: 10, price: '8,560' },
-    { date: '19 Oct', value: 15, price: '4,200' },
-    { date: '19 Oct', value: 30, price: '1,200' },
-    { date: '20 Oct', value: 10, price: '540' },
-    { date: '20 Oct', value: 25, price: '9,900' },
-    { date: '20 Oct', value: 5, price: '7,520' },
-    { date: '20 Oct', value: 20, price: '3,450' },
-    { date: '21 Oct', value: 15, price: '890' },
-    { date: '21 Oct', value: 28, price: '6,120' },
-    { date: '21 Oct', value: 3, price: '500' },
-    { date: '21 Oct', value: 22, price: '9,550' },
-    { date: '22 Oct', value: 18, price: '1,500' },
-    { date: '22 Oct', value: -5, price: '4,300' },
-    { date: '22 Oct', value: 14, price: '8,700' },
-    { date: '22 Oct', value: 25, price: '900' },
-    { date: '23 Oct', value: 10, price: '2,100' },
-    { date: '23 Oct', value: 35, price: '7,800' },
-    { date: '23 Oct', value: 8, price: '50' },
-    { date: '23 Oct', value: 28, price: '6,540' },
-    { date: '24 Oct', value: 15, price: '1,000' },
-    { date: '24 Oct', value: -10, price: '3,500' },
-    { date: '24 Oct', value: 32, price: '9,999' },
-    { date: '24 Oct', value: 20, price: '4,250' },
+  { date: '18 Oct', fullDate: 'Fri-18 Oct, 2021', value: 7, price: '9,865' },
+  { date: '18 Oct', fullDate: 'Fri-18 Oct, 2021', value: -1, price: '3,896' },
+  { date: '18 Oct', fullDate: 'Fri-18 Oct, 2021', value: 12, price: '100' },
+  { date: '18 Oct', fullDate: 'Fri-18 Oct, 2021', value: 8, price: '7,084' },
+  { date: '19 Oct', fullDate: 'Sat-19 Oct, 2021', value: 5, price: '250' },
+  { date: '19 Oct', fullDate: 'Sat-19 Oct, 2021', value: 10, price: '8,560' },
+  { date: '19 Oct', fullDate: 'Sat-19 Oct, 2021', value: 15, price: '4,200' },
+  { date: '19 Oct', fullDate: 'Sat-19 Oct, 2021', value: 30, price: '1,200' },
+  { date: '20 Oct', fullDate: 'Sun-20 Oct, 2021', value: 10, price: '540' },
+  { date: '20 Oct', fullDate: 'Sun-20 Oct, 2021', value: 25, price: '9,900' },
+  { date: '20 Oct', fullDate: 'Sun-20 Oct, 2021', value: 5, price: '7,520' },
+  { date: '20 Oct', fullDate: 'Sun-20 Oct, 2021', value: 20, price: '3,450' },
+  { date: '21 Oct', fullDate: 'Mon-21 Oct, 2021', value: 15, price: '890' },
+  { date: '21 Oct', fullDate: 'Mon-21 Oct, 2021', value: 28, price: '6,120' },
+  { date: '21 Oct', fullDate: 'Mon-21 Oct, 2021', value: 3, price: '500' },
+  { date: '21 Oct', fullDate: 'Mon-21 Oct, 2021', value: 22, price: '9,550' },
+  { date: '22 Oct', fullDate: 'Tue-22 Oct, 2021', value: 18, price: '1,500' },
+  { date: '22 Oct', fullDate: 'Tue-22 Oct, 2021', value: -5, price: '4,300' },
+  { date: '22 Oct', fullDate: 'Tue-22 Oct, 2021', value: 14, price: '8,700' },
+  { date: '22 Oct', fullDate: 'Tue-22 Oct, 2021', value: 25, price: '900' },
+  { date: '23 Oct', fullDate: 'Wed-23 Oct, 2021', value: 10, price: '2,100' },
+  { date: '23 Oct', fullDate: 'Wed-23 Oct, 2021', value: 35, price: '7,800' },
+  { date: '23 Oct', fullDate: 'Wed-23 Oct, 2021', value: 8, price: '50' },
+  { date: '23 Oct', fullDate: 'Wed-23 Oct, 2021', value: 28, price: '6,540' },
+  { date: '24 Oct', fullDate: 'Thu-24 Oct, 2021', value: 15, price: '1,000' },
+  { date: '24 Oct', fullDate: 'Thu-24 Oct, 2021', value: -10, price: '3,500' },
+  { date: '24 Oct', fullDate: 'Thu-24 Oct, 2021', value: 32, price: '9,999' },
+  { date: '24 Oct', fullDate: 'Thu-24 Oct, 2021', value: 20, price: '4,250' },
 ];
 
 const CustomTooltip = ({ active, payload, label }: TooltipProps<ValueType, NameType>) => {
@@ -44,12 +44,13 @@ const CustomTooltip = ({ active, payload, label }: TooltipProps<ValueType, NameT
     const dataPoint = portfolioData.find(d => d.date === label && d.value === payload[0].value);
     
     const formattedPrice = dataPoint ? `$${dataPoint.price}.00` : 'N/A';
+    const formattedDate = dataPoint ? dataPoint.fullDate : label;
 
     return (
       <div className='bg-neutral-900 rounded-[8px]'>
         <div className="bg-white-neutral-800 px-2 py-1 rounded-[8px] border border-white-neutral-700">
           <p className="text-[12px] leading-[165%] font-semibold text-base-white">{formattedPrice}</p>
-          <p className="text-[12px] leading-[165%] font-medium text-white-neutral-400">{label}</p>
+          <p className="text-[12px] leading-[165%] font-medium text-white-neutral-400">{formattedDate}</p>
         </div>
       </div>
     );
@@ -61,16 +62,16 @@ const PortfolioValue = () => {
     const [activeTime, setActiveTime] = useState('1W');
 
     const chartConfig: ChartConfig = {
-        value: {
-            label: 'Portfolio Value',
-            color: '#ffff',
-        },
+      value: {
+        label: 'Portfolio Value',
+        color: '#ffff',
+      },
     };
 
     const uniqueDates = Array.from(new Set(portfolioData.map(d => d.date)));
 
     return (
-        <Card className="bg-white-neutral-800 bg-[url('/src/assets/overview/chart.png')] bg-no-repeat bg-cover bg-bottom text-white border-none shadow-none w-full h-full">
+        <Card className="bg-neutral-900 bg-[url('/src/assets/overview/chart.png')] bg-no-repeat bg-cover bg-bottom text-white border-none shadow-none w-full h-full">
             <CardHeader className="flex flex-row items-start justify-between w-full">
                 <div className="flex flex-row gap-5 items-center">
                     <div className="flex flex-col items-start gap-[2px] border-b pb-[7px] border-base-white text-base-white">
@@ -103,6 +104,15 @@ const PortfolioValue = () => {
             <CardContent className="w-full">
                 <ChartContainer config={chartConfig} className="h-[196px] w-full">
                     <LineChart data={portfolioData}>
+                        <defs>
+                            <pattern id="dotted-pattern" x="0" y="0" width="12" height="12" patternUnits="userSpaceOnUse">
+                                <circle cx="2" cy="2" r="1.2" fill="#FFFFFF14" />
+                            </pattern>
+                        </defs>
+                        <CartesianGrid 
+                            stroke='none'
+                            fill='url(#dotted-pattern)'
+                        />
                         <XAxis
                             dataKey="date"
                             ticks={uniqueDates}
