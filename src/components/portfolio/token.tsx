3b4f70f362cb68f3c2b412e4676da7117c6ad4ec
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-09-09 15:28:35 +0700

    style: add animation to token chart

--- src/components/portfolio/token.tsx
+++ src/components/portfolio/token.tsx
@@ -1,12 +1,44 @@
+import { motion } from "framer-motion"
+
 export default function TokenAllocation() {
     return (
         <div className="bg-white-neutral-900 border border-white-neutral-800 rounded-[12px] flex flex-col h-full">
             <div className="px-5 pt-5 pb-4 text-[16px] leading-[160%] text-base-white font-medium">Token Allocation</div>
             <div className="flex flex-col gap-4 px-5 pb-5">
                 <div className="flex flex-row py-[17px] gap-[3px]">
-                    <div className="w-full max-w-[205px] h-8 rounded-[4px] bg-primary-400"/>
-                    <div className="w-full max-w-20 h-8 rounded-[4px] bg-success-400"/>
-                    <div className="w-full max-w-14 h-8 rounded-[4px] bg-secondary-400"/>
+                    <motion.div
+                        className="w-full max-w-[205px] h-8 rounded-[4px] bg-primary-400"
+                        initial={{ scaleX: 0 }}
+                        animate={{ scaleX: 1 }}
+                        transition={{ 
+                        delay: 0,
+                        duration: 0.8,
+                        ease: "easeInOut"
+                        }}
+                        style={{ originX: 0 }}
+                    />
+                    <motion.div
+                        className="w-full max-w-20 h-8 rounded-[4px] bg-success-400"
+                        initial={{ scaleX: 0 }}
+                        animate={{ scaleX: 1 }}
+                        transition={{ 
+                        delay: 0.6,
+                        duration: 0.8,
+                        ease: "easeInOut"
+                        }}
+                        style={{ originX: 0 }}
+                    />
+                    <motion.div
+                        className="w-full max-w-14 h-8 rounded-[4px] bg-secondary-400"
+                        initial={{ scaleX: 0 }}
+                        animate={{ scaleX: 1 }}
+                        transition={{ 
+                        delay: 0.9,
+                        duration: 0.8,
+                        ease: "easeInOut"
+                        }}
+                        style={{ originX: 0 }}
+                    />
                 </div>
                 <div className="flex flex-row gap-4">
                     <div className="flex flex-col gap-1.5 w-24">
