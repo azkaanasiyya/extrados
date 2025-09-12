756e4a7f667e954e3ad529382427527bd1ff8070
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-09-12 15:33:04 +0700

    style: update tokens to table using component shadcn

--- src/components/overview/tokens.tsx
+++ src/components/overview/tokens.tsx
@@ -2,6 +2,7 @@ import { Columns3Cog, Filter } from "lucide-react";
 import { Button } from "../ui/button";
 import type React from "react";
 import { TokensData } from "@/data/token";
+import { TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
 
 export default function Tokens() {
     return (
@@ -22,45 +23,49 @@ export default function Tokens() {
                     </Button>
                 </div>
             </div>
-            <div className="px-5 border-b border-white-neutral-800 flex flex-row">
-                <div className="py-2.5 px-2 w-full max-w-[208px] text-start text-[12px] leading-[165%] text-white-neutral-400 font-medium">NAME</div>
-                <div className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">PRICE</div>
-                <div className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">BALANCE</div>
-                <div className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">MARKET CAP</div>
-                <div className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">VOLUME (24H)</div>
-                <div className="py-2.5 px-6 w-full max-w-[216px] text-start text-[12px] leading-[165%] text-white-neutral-400 font-medium">CIRC SUPPLY</div>
-            </div>
-            {TokensData.slice(0, 5).map((token, index) => {
-                const allBars: React.ReactNode[] = [];
-                token.bars.forEach(group => {
-                    for (let i = 0; i < group.count; i++) {
-                        allBars.push(
-                            <div
-                                key={`${token.name}-${allBars.length}`}
-                                className={`w-0.5 h-5 rounded-[8px] ${group.color}`}
-                            />
-                        );
-                    }
-                });
-                return (
-                    <div key={index} className="mx-5 border-b border-white-neutral-800 flex flex-row items-center">
-                        <div className="py-2.5 px-2 w-full max-w-[208px] items-center justify-start flex flex-row gap-2">
-                            <img src={token.image} alt={token.name} className="w-9 h-9" />
-                            <span className="text-[14px] leading-[160%] text-base-white font-bold">{token.name}</span>
-                        </div>
-                        <div className="py-4 px-2 w-full max-w-[184px] text-end text-[14px] leading-[160%] text-base-white font-bold">{token.price}</div>
-                        <div className="py-4 px-2 w-full max-w-[184px] text-end text-[14px] leading-[160%] text-base-white font-bold">{token.balance}</div>
-                        <div className="py-4 px-2 w-full max-w-[184px] text-end text-[14px] leading-[160%] text-base-white font-bold">{token.marketCap}</div>
-                        <div className="py-4 px-2 w-full max-w-[184px] text-end text-[14px] leading-[160%] text-base-white font-bold">{token.volume}</div>
-                        <div className="py-4 px-6 w-full max-w-[216px] flex flex-row gap-1.5 items-center">
-                            <div className="flex flex-row gap-[3px]">
-                                {allBars}
-                            </div>
-                            <span className="text-[14px] leading-[160%] font-medium text-base-white">{token.change}</span>
-                        </div>
-                    </div>
-                )
-            })}
+            <TableHeader>
+                <TableRow className="px-5 border-b border-white-neutral-800 flex flex-row hover:bg-transparent">
+                    <TableHead className="py-2.5 px-2 w-full max-w-[208px] text-start text-[12px] leading-[165%] text-white-neutral-400 font-medium">NAME</TableHead>
+                    <TableHead className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">PRICE</TableHead>
+                    <TableHead className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">BALANCE</TableHead>
+                    <TableHead className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">MARKET CAP</TableHead>
+                    <TableHead className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">VOLUME (24H)</TableHead>
+                    <TableHead className="py-2.5 px-6 w-full max-w-[216px] text-start text-[12px] leading-[165%] text-white-neutral-400 font-medium">CIRC SUPPLY</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {TokensData.slice(0, 5).map((token, index) => {
+                    const allBars: React.ReactNode[] = [];
+                    token.bars.forEach(group => {
+                        for (let i = 0; i < group.count; i++) {
+                            allBars.push(
+                                <div
+                                    key={`${token.name}-${allBars.length}`}
+                                    className={`w-0.5 h-5 rounded-[8px] ${group.color}`}
+                                />
+                            );
+                        }
+                    });
+                    return (
+                        <TableRow key={index} className="hover:bg-transparent mx-5 border-b border-white-neutral-800 flex flex-row items-center">
+                            <TableCell className="py-2.5 px-2 w-full max-w-[208px] items-center justify-start flex flex-row gap-2">
+                                <img src={token.image} alt={token.name} className="w-9 h-9" />
+                                <span className="text-[14px] leading-[160%] text-base-white font-bold">{token.name}</span>
+                            </TableCell>
+                            <TableCell className="py-4 px-2 w-full max-w-[184px] text-end text-[14px] leading-[160%] text-base-white font-bold">{token.price}</TableCell>
+                            <TableCell className="py-4 px-2 w-full max-w-[184px] text-end text-[14px] leading-[160%] text-base-white font-bold">{token.balance}</TableCell>
+                            <TableCell className="py-4 px-2 w-full max-w-[184px] text-end text-[14px] leading-[160%] text-base-white font-bold">{token.marketCap}</TableCell>
+                            <TableCell className="py-4 px-2 w-full max-w-[184px] text-end text-[14px] leading-[160%] text-base-white font-bold">{token.volume}</TableCell>
+                            <TableCell className="py-4 px-6 w-full max-w-[216px] flex flex-row gap-1.5 items-center">
+                                <div className="flex flex-row gap-[3px]">
+                                    {allBars}
+                                </div>
+                                <span className="text-[14px] leading-[160%] font-medium text-base-white">{token.change}</span>
+                            </TableCell>
+                        </TableRow>
+                    )
+                })}
+            </TableBody>
         </div>
     )
 }
