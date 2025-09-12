6ac9a044435c1e3dc6a85990b7cdcabea485c5b5
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-09-12 17:02:24 +0700

    style: update table using component table shadcn

--- src/pages/pools.tsx
+++ src/pages/pools.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Columns3Cog, Filter } from "lucide-react";
 import { PoolsData } from "@/data/pool";
 import { Link } from "react-router-dom";
+import { TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export default function Pools() {
     return (
@@ -19,37 +20,42 @@ export default function Pools() {
                     </Button>
                 </div>
             </div>
-            <div className="px-5 border-b border-white-neutral-800 flex flex-row">
-                <div className="py-2.5 px-2 w-full max-w-[424px] text-start text-[12px] leading-[165%] text-white-neutral-400 font-medium">NAME</div>
-                <div className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">DETAILS / %CHANGE</div>
-                <div className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">TVL</div>
-                <div className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">VOLUME (24H)</div>
-                <div className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">APR</div>
-            </div>
-            {PoolsData.map((pool, index, arr) => (
-                <Link key={index} to={`/pools/details`}>
-                   <div key={index} className={`mx-5 flex flex-row items-center cursor-pointer hover:bg-white-neutral-800 ${index === arr.length - 1 ? '' : 'border-b border-white-neutral-800'}`}>
-                        <div className="px-2 py-4 flex flex-row items-center gap-2 w-full max-w-[424px]">
-                            <div className="p-3 py-[7px] flex flex-row items-center gap-2 bg-white-neutral-900 border border-white-neutral-800 rounded-[8px]">
-                                <img src={pool.token1.image} alt={pool.token1.name} width={16} height={16} />
-                                <span className="text-[14px] leading-[160%] text-base-white font-bold">{pool.token1.name}</span>
-                            </div>
-                            <span className="font-medium text-white-neutral-300 text-[16px] leading-[160%]">/</span>
-                            <div className="p-3 py-[7px] flex flex-row items-center gap-2 bg-white-neutral-900 border border-white-neutral-800 rounded-[8px]">
-                                <img src={pool.token2.image} alt={pool.token2.name} width={16} height={16} />
-                                <span className="text-[14px] leading-[160%] text-base-white font-bold">{pool.token2.name}</span>
-                            </div>
-                        </div>
-                        <div className="px-2 py-4 flex flex-row gap-2 justify-end items-center w-full max-w-[184px]">
-                            <div className="px-2.5 py-[5px] rounded-[8px] bg-white-neutral-900 text-[14px] leading-[160%] font-bold text-base-white">{pool.version}</div>
-                            <span className="text-[14px] leading-[160%] font-bold text-white-neutral-300">{pool.change}</span>
-                        </div>
-                        <div className="px-2 py-4 text-end w-full max-w-[184px] font-bold text-[14px] leading-[160%] text-base-white">{pool.tvl}</div>
-                        <div className="px-2 py-4 text-end w-full max-w-[184px] font-bold text-[14px] leading-[160%] text-base-white">{pool.volume}</div>
-                        <div className="px-2 py-4 text-end w-full max-w-[184px] font-bold text-[14px] leading-[160%] text-base-white">{pool.apr}</div>
-                    </div> 
-                </Link>
-            ))}
+            <TableHeader>
+                <TableRow className="px-5 border-b border-white-neutral-800 flex flex-row hover:bg-transparent">
+                    <TableHead className="py-2.5 px-2 w-full max-w-[424px] text-start text-[12px] leading-[165%] text-white-neutral-400 font-medium">NAME</TableHead>
+                    <TableHead className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">DETAILS / %CHANGE</TableHead>
+                    <TableHead className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">TVL</TableHead>
+                    <TableHead className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">VOLUME (24H)</TableHead>
+                    <TableHead className="py-2.5 px-2 w-full max-w-[184px] text-end text-[12px] leading-[165%] text-white-neutral-400 font-medium">APR</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {PoolsData.map((pool, index, arr) => (
+                    <Link key={index} to={`/pools/details`} className={`mx-5 flex flex-row items-center cursor-pointer ${index === arr.length - 1 ? '' : 'border-b border-white-neutral-800'}`}>
+                        <TableRow key={index} className={`flex flex-row items-center w-full hover:bg-white-neutral-800`}>
+                            <TableCell className="px-2 py-4 flex flex-row items-center gap-2 w-full max-w-[424px]">
+                                <div className="p-3 py-[7px] flex flex-row items-center gap-2 bg-white-neutral-900 border border-white-neutral-800 rounded-[8px]">
+                                    <img src={pool.token1.image} alt={pool.token1.name} width={16} height={16} />
+                                    <span className="text-[14px] leading-[160%] text-base-white font-bold">{pool.token1.name}</span>
+                                </div>
+                                <span className="font-medium text-white-neutral-300 text-[16px] leading-[160%]">/</span>
+                                <div className="p-3 py-[7px] flex flex-row items-center gap-2 bg-white-neutral-900 border border-white-neutral-800 rounded-[8px]">
+                                    <img src={pool.token2.image} alt={pool.token2.name} width={16} height={16} />
+                                    <span className="text-[14px] leading-[160%] text-base-white font-bold">{pool.token2.name}</span>
+                                </div>
+                            </TableCell>
+                            <TableCell className="px-2 py-4 flex flex-row gap-2 justify-end items-center w-full max-w-[184px]">
+                                <div className="px-2.5 py-[5px] rounded-[8px] bg-white-neutral-900 text-[14px] leading-[160%] font-bold text-base-white">{pool.version}</div>
+                                <span className="text-[14px] leading-[160%] font-bold text-white-neutral-300">{pool.change}</span>
+                            </TableCell>
+                            <TableCell className="px-2 py-4 text-end w-full max-w-[184px] font-bold text-[14px] leading-[160%] text-base-white">{pool.tvl}</TableCell>
+                            <TableCell className="px-2 py-4 text-end w-full max-w-[184px] font-bold text-[14px] leading-[160%] text-base-white">{pool.volume}</TableCell>
+                            <TableCell className="px-2 py-4 text-end w-full max-w-[184px] font-bold text-[14px] leading-[160%] text-base-white">{pool.apr}</TableCell>
+                        </TableRow> 
+                    </Link>
+                ))}
+            </TableBody>
+            
         </div>
     )
 }
